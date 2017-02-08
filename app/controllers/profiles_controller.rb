c1515c7800a715dedfb66bf5588ff7f90ac25c14
Shane Castile <paperscratcher@gmail.com>
2017-02-08 23:45:44 +0000

    Updated Added avatar column to profiles table

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,6 @@ class ProfilesController < ApplicationController
   
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_name, :description)
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_name, :description)
     end
 end
